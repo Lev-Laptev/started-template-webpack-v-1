3e7b811e2b96710cd02a5eff806d282ac8f405d6
Lev <47007280+Lev-Laptev@users.noreply.github.com>
2022-06-15 18:56:47 +0300

    feat: template webpack part 1

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let mode = 'development';
+if(process.env.NODE_ENV === 'production') {
+    mode = 'production'
+}
+
+module.exports = {
+    mode: mode,
+    entry: {
+        scripts: './src/js/index.js',
+    },
+    output: {
+        filename: 'js/[name].[contenthash].js',
+        assetModuleFilename: "assets/[hash:6][ext][query]",
+        clean: true,
+    },
+    devServer: {
+        open: true,
+        static: {
+            directory: './src',
+            watch: true
+        }
+    },
+    devtool: 'source-map',
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        })],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    mode !== 'production'
+                    ? 'style-loader'
+                    : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            // Options
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name].[hash:6][ext]',
+                },
+                
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    // options: {
+                    //     presets: ['@babel/preset-env']
+                    // }
+                }
+            }
+        ]
+    },
+}
